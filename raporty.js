3cf70b9dd4f4b93db7c7d4d2be046fb42fd42fe3
shenjzs <kuba.dobrzanski@wp.pl>
2026-05-10 12:24:21 +0000

    Deploying to gh-pages from @ shenjzs/test@9eb8dc63292fe7b2728d09e29e63f9b4d525960d 🚀

--- raporty.js
+++ raporty.js
@@ -1,7 +1,7 @@
 // ==========================================
 // WERSJA APLIKACJI (Zmień, aby wymusić odświeżenie u wszystkich)
 // ==========================================
-const APP_VERSION = "1.3.2";
+const APP_VERSION = "1.3.3";
 
 // ==========================================
 // KONFIGURACJA LINKÓW I CEN
